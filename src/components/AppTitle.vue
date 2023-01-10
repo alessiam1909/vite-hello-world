0a15ee9c8ab0c62800b351e933e4bfb53e24ad4a
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-10 21:11:56 +0100

    creazione titolo esercizio base

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+    name: "AppTitle",
+    data(){
+        return{
+            title : "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+
+<template lang="">
+    <div class="title">
+        <h1>
+            {{title}}
+        </h1>
+    </div>
+</template>
+
+
+<style lang="">
+
+
+</style>
