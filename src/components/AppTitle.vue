087e1115fe62f5b96fa7965022982a6b6b8fe962
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-11 10:16:09 +0100

    stilizzazione titolo

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -20,7 +20,18 @@ export default{
 </template>
 
 
-<style lang="">
+<style>
 
+    .title{
+        background-color: rgb(175, 137, 211);
+        padding: 20px;
+        border-radius: 20px;
+        border: 1px solid black;
+        box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);
+    }
+    h1{
+        color: rgb(123, 219, 219);
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    }
 
 </style>
